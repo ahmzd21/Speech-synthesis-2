63f1915483b1ccd2a32f29eab0b451a537944f2b
ahmzd21 <ziaa4847@gmail.com>
2024-12-26 12:04:51 +0500

    prepare to commit

--- speech synthesis - Copy/api/models/History.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const HistorySchema = new mongoose.Schema({
-  text: String,
-  audioPath: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("History", HistorySchema);
